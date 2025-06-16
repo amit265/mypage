3299a1a5cda182fec2c61418034464687e8f96b9
Amit kumar <amitk.kumar414@gmail.com>
2025-06-17 00:36:12 +0530

    project page in making

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -1,107 +1,65 @@
 import Image from "next/image";
+import { projects } from "@/constants/projects";
 
 export default function ProjectsSection() {
   return (
-    <section className="h-screen overflow-y-auto p-8">
-      <h2 className="text-xl font-semibold mb-6 text-left text-white">WHAT I DO</h2>
+    <section className="h-screen overflow-y-auto">
+      <div className="p-8" >
+        <h2 className="text-xl font-semibold mb-6 text-left text-white">WHAT I DO</h2>
+        <h2 className="text-4xl font-semibold mb-6 text-left text-[#BDF347]">Projects</h2>
+      </div>
 
-      <h2 className="text-4xl font-semibold mb-6 text-left text-[#BDF347]">Projects</h2>
+      <div className="flex flex-col mt-8">
+        {projects.map((project, index) => (
+          <div key={project?.id} className="flex h-[60vh] p-8 gap-8">
+            {/* Alternate Layout */}
+            {index % 2 !== 0 ? (
+              <div className="flex flex-row gap-8 items-end justify-center">
 
-      <div className="flex flex-col">
-        <div className="flex flex-row p-4">
-          <div className="w-1/2 text-white space-y-4 ">
-            <h2 className="text-white font-bold text-xl">CodeRespite: Refresh Your Tech Skills </h2>
-            <p>
-              A self-paced mobile learning app covering HTML, CSS, JavaScript, Node.js, React, and React Native. Features include reading lessons, quiz modules, progress tracking.
-            </p>
-            <p>Tech Stake: React Native, Firebase, AsyncStorage, JavaScript .</p>
+                <div className="w-1/2 -ml-12">
+                  <Image
+                    src="/images/light.png"
+                    width={500}
+                    height={500}
+                    alt="Picture of the author"
+                    className="-ml-86"
+                  />
+                  <Image
+                    src={`/images/projects/${project?.image}.png`}
+                    width={800}
+                    height={800}
+                    alt={project?.title}
+                    className="w-full h-[50vh] rounded-lg shadow-lg object-cover"
+                  />
+                </div>
+                <div className="md:w-1/2 w-full text-white space-y-4 p-12">
+                  <h3 className="text-2xl font-bold">{project?.title}</h3>
+                  <p className="text-sm text-[#BDF347]">{project?.techStack}</p>
+                  <p className="text-base leading-relaxed ">{project?.description}</p>
+                </div>
+              </div>
+            ) : (
+              <div className="flex flex-row gap-8 items-end justify-center p-12">
+                <div className="md:w-1/2 w-full text-white space-y-4">
+                  <h3 className="text-2xl font-bold">{project?.title}</h3>
+                  <p className="text-sm text-[#BDF347]">{project?.techStack}</p>
+                  <p className="text-base leading-relaxed text-justify">{project?.description}</p>
+                </div>
+                <div className="w-1/2 -mr-32">
+                  <Image
+                    src={`/images/projects/${project?.image}.png`}
+                    width={800}
+                    height={800}
+                    alt={project?.title}
+                    className="w-full h-[50vh] rounded-lg shadow-lg object-cover"
+                  />
+                </div>
+              </div>
+            )}
           </div>
-          <div className="w-1/2">
-            <Image
-              src="/images/light.png"
-              width={500}
-              height={500}
-              alt="Picture of the author"
-              className="w-1/2 mx-auto"
-            />
-          </div>
-        </div>
-        <div className="flex flex-row p-4">
-
-          <div className="w-1/2">
-            <Image
-              src="/images/light.png"
-              width={500}
-              height={500}
-              alt="Picture of the author"
-              className="w-1/2 mx-auto"
-            />
-          </div>
-          <div className="w-1/2 text-white space-y-4 ">
-            <h2 className="text-white font-bold text-xl">CodeRespite: Refresh Your Tech Skills </h2>
-            <p>
-              A self-paced mobile learning app covering HTML, CSS, JavaScript, Node.js, React, and React Native. Features include reading lessons, quiz modules, progress tracking.
-            </p>
-            <p>Tech Stake: React Native, Firebase, AsyncStorage, JavaScript .</p>
-          </div>
-        </div>
-        <div className="flex flex-row p-4">
-          <div className="w-1/2 text-white space-y-4 ">
-            <h2 className="text-white font-bold text-xl">CodeRespite: Refresh Your Tech Skills </h2>
-            <p>
-              A self-paced mobile learning app covering HTML, CSS, JavaScript, Node.js, React, and React Native. Features include reading lessons, quiz modules, progress tracking.
-            </p>
-            <p>Tech Stake: React Native, Firebase, AsyncStorage, JavaScript .</p>
-          </div>
-          <div className="w-1/2">
-            <Image
-              src="/images/light.png"
-              width={500}
-              height={500}
-              alt="Picture of the author"
-              className="w-1/2 mx-auto"
-            />
-          </div>
-        </div>
-        <div className="flex flex-row p-4">
-        <div className="w-1/2">
-            <Image
-              src="/images/light.png"
-              width={500}
-              height={500}
-              alt="Picture of the author"
-              className="w-1/2 mx-auto"
-            />
-          </div>
-          <div className="w-1/2 text-white space-y-4 ">
-            <h2 className="text-white font-bold text-xl">CodeRespite: Refresh Your Tech Skills </h2>
-            <p>
-              A self-paced mobile learning app covering HTML, CSS, JavaScript, Node.js, React, and React Native. Features include reading lessons, quiz modules, progress tracking.
-            </p>
-            <p>Tech Stake: React Native, Firebase, AsyncStorage, JavaScript .</p>
-          </div>
-         
-        </div>
-        <div className="flex flex-row p-4">
-          <div className="w-1/2 text-white space-y-4 ">
-            <h2 className="text-white font-bold text-xl">CodeRespite: Refresh Your Tech Skills </h2>
-            <p>
-              A self-paced mobile learning app covering HTML, CSS, JavaScript, Node.js, React, and React Native. Features include reading lessons, quiz modules, progress tracking.
-            </p>
-            <p>Tech Stake: React Native, Firebase, AsyncStorage, JavaScript .</p>
-          </div>
-          <div className="w-1/2">
-            <Image
-              src="/images/light.png"
-              width={500}
-              height={500}
-              alt="Picture of the author"
-              className="w-1/2 mx-auto"
-            />
-          </div>
-        </div>
-
+        ))}
       </div>
     </section>
+
   );
 }
